4461fb06d4a395bf2fc0e019747da87671f50fb2
Jose Gomez <k13gomez@gmail.com>
2019-08-25 00:10:40 -0400

    add sg, acl, rt

--- resources.tf
+++ resources.tf
@@ -27,7 +27,7 @@ provider "aws" {
   region  = var.env_region
 }
 
-resource "aws_vpc" "stack-private" {
+resource "aws_vpc" "private" {
   cidr_block = "${var.private_network_cidr_base}.0.0/16"
 
   tags = {
@@ -35,7 +35,33 @@ resource "aws_vpc" "stack-private" {
   }
 }
 
-resource "aws_vpc_dhcp_options" "stack-private" {
+resource "aws_security_group" "private-sg" {
+  vpc_id      = "${aws_vpc.private.id}"
+  name        = "${var.stack_name}-private-sg-${var.env}"
+  description = "${var.stack_name} private security group - ${var.env}"
+
+  tags = {
+    Name = "${var.stack_name}-private-sg-${var.env}"
+  }
+}
+
+resource "aws_network_acl" "private-acl" {
+  vpc_id     = "${aws_vpc.private.id}"
+  subnet_ids = ["${aws_subnet.private-subnet-a.id}", "${aws_subnet.private-subnet-b.id}", "${aws_subnet.private-subnet-c.id}", "${aws_subnet.private-subnet-d.id}"]
+
+  tags = {
+    Name = "${var.stack_name}-private-acl-${var.env}"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.private.id}"
+  tags = {
+    Name = "${var.stack_name}-route-table-${var.env}"
+  }
+}
+
+resource "aws_vpc_dhcp_options" "private" {
   domain_name_servers = ["AmazonProvidedDNS"]
   domain_name         = "ec2.internal"
 
@@ -44,13 +70,13 @@ resource "aws_vpc_dhcp_options" "stack-private" {
   }
 }
 
-resource "aws_vpc_dhcp_options_association" "stack-private" {
-  vpc_id          = "${aws_vpc.stack-private.id}"
-  dhcp_options_id = "${aws_vpc_dhcp_options.stack-private.id}"
+resource "aws_vpc_dhcp_options_association" "private" {
+  vpc_id          = "${aws_vpc.private.id}"
+  dhcp_options_id = "${aws_vpc_dhcp_options.private.id}"
 }
 
-resource "aws_subnet" "stack-private-subnet-a" {
-  vpc_id            = "${aws_vpc.stack-private.id}"
+resource "aws_subnet" "private-subnet-a" {
+  vpc_id            = "${aws_vpc.private.id}"
   cidr_block        = "${var.private_network_cidr_base}.0.0/23"
   availability_zone = "${var.env_region}a"
 
@@ -59,8 +85,8 @@ resource "aws_subnet" "stack-private-subnet-a" {
   }
 }
 
-resource "aws_subnet" "stack-private-subnet-b" {
-  vpc_id            = "${aws_vpc.stack-private.id}"
+resource "aws_subnet" "private-subnet-b" {
+  vpc_id            = "${aws_vpc.private.id}"
   cidr_block        = "${var.private_network_cidr_base}.2.0/23"
   availability_zone = "${var.env_region}b"
 
@@ -69,8 +95,8 @@ resource "aws_subnet" "stack-private-subnet-b" {
   }
 }
 
-resource "aws_subnet" "stack-private-subnet-c" {
-  vpc_id            = "${aws_vpc.stack-private.id}"
+resource "aws_subnet" "private-subnet-c" {
+  vpc_id            = "${aws_vpc.private.id}"
   cidr_block        = "${var.private_network_cidr_base}.4.0/23"
   availability_zone = "${var.env_region}c"
 
@@ -79,8 +105,8 @@ resource "aws_subnet" "stack-private-subnet-c" {
   }
 }
 
-resource "aws_subnet" "stack-private-subnet-d" {
-  vpc_id            = "${aws_vpc.stack-private.id}"
+resource "aws_subnet" "private-subnet-d" {
+  vpc_id            = "${aws_vpc.private.id}"
   cidr_block        = "${var.private_network_cidr_base}.6.0/23"
   availability_zone = "${var.env_region}d"
 
