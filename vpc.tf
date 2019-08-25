857cf622522832af92339568d677539f7c781b96
Jose Gomez <k13gomez@gmail.com>
2019-08-25 15:40:36 -0400

    cleanup

--- vpc.tf
+++ vpc.tf
@@ -19,25 +19,3 @@ resource "aws_vpc_dhcp_options_association" "network-vpc-dhcp-options" {
   vpc_id          = "${aws_vpc.network_vpc.id}"
   dhcp_options_id = "${aws_vpc_dhcp_options.dhcp_options.id}"
 }
-
-resource "aws_default_network_acl" "default" {
-  default_network_acl_id = "${aws_vpc.network_vpc.default_network_acl_id}"
-
-  ingress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "${var.network_vpc_cidr}"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  egress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-}
