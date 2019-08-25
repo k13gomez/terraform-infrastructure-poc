9adca5a41de5d9d2bf764872aa2af65bfe08b1f6
Jose Gomez <k13gomez@gmail.com>
2019-08-25 13:08:15 -0400

    add private route table associations

--- private-routes.tf
+++ private-routes.tf
@@ -5,3 +5,23 @@ resource "aws_route_table" "private_rt" {
     Name = "${var.stack_name}-route-table"
   }
 }
+
+resource "aws_route_table_association" "private_subnet_a_rt" {
+  subnet_id      = "${aws_subnet.private_subnet_a.id}"
+  route_table_id = "${aws_route_table.private_rt.id}"
+}
+
+resource "aws_route_table_association" "private_subnet_b_rt" {
+  subnet_id      = "${aws_subnet.private_subnet_b.id}"
+  route_table_id = "${aws_route_table.private_rt.id}"
+}
+
+resource "aws_route_table_association" "private_subnet_c_rt" {
+  subnet_id      = "${aws_subnet.private_subnet_c.id}"
+  route_table_id = "${aws_route_table.private_rt.id}"
+}
+
+resource "aws_route_table_association" "private_subnet_d_rt" {
+  subnet_id      = "${aws_subnet.private_subnet_d.id}"
+  route_table_id = "${aws_route_table.private_rt.id}"
+}
